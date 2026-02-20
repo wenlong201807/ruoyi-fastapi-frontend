861ad4dcc8bdceff6dd1d759d229a9644384718b
朱文龙 <1573511441@qq.com>
2026-02-20 17:04:33 +0800

    feat: env default

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>前端--环境变量：{{ import.meta.env.VITE_APP_TITLE }}</h1>
+    <h1>前端--环境变量：{{ title }}</h1>
     <router-view />
   </div>
 </template>
@@ -8,6 +8,8 @@
 <script setup>
 import useSettingsStore from '@/store/modules/settings'
 import { handleThemeStyle } from '@/utils/theme'
+import { ref } from 'vue'
+const title = ref(import.meta.env.VITE_APP_TITLE)
 
 onMounted(() => {
   nextTick(() => {
